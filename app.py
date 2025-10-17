2e4c3399c8db75121cac41b44572d12cc6263918
vinney560 <vinneyjoy1@gmail.com>
2025-10-17 10:13:38 +0300

    Gen

--- app.py
+++ app.py
@@ -2849,4 +2849,4 @@ def internal_error(error):
 
 # ==========================================
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=47947, debug=True)
+    socketio.run(app, host='0.0.0.0', port=47947, debug=False)
