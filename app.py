6817fc58b467b9b41b802bc874cd9a57f9835694
vinney560 <vinneyjoy1@gmail.com>
2025-11-29 16:37:03 +0300

    Update url pinging

--- app.py
+++ app.py
@@ -3308,15 +3308,13 @@ def check():
 @app.route('/logout')
 @login_required
 def logout():
-    print(f"Before logout - User authenticated: {current_user.is_authenticated}")
     session.pop('_flashes', None)
     session.pop('_user_id', None)
     session.pop('user_id', None)
-    session.pop('is_authenticated', None)
+    session.pop('is_authenticated', False)
     session.clear()
     session.modified = True
     logout_user()
-    print(f"After logout - User authenticated: {current_user.is_authenticated}")
     flash('Logout Successfully!', 'success')
     return redirect(url_for('login'))
 #--------------------------------------------------------------------
