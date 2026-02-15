1909b6096f4b95ae656105e7ff239b64259dcf8a
vinney560 <vinneyjoy1@gmail.com>
2026-02-15 20:46:19 +0300

    general updates

--- test10.py
+++ test10.py
@@ -1,8 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 URL = 'https://lyxnexus.onrender.com/main-page'
 response = requests.get(URL)
 if response.status_code == 200:
     print("Successfully accessed the main page!")
-    print(response.text)
+    soup = BeautifulSoup(response.text, 'html.parser')
+    print("\nPRETTY HTML TITLE:")
+    print(soup.title.string if soup.title else "No title found")
 else:
     print(f"Failed to access the main page. Status code: {response.status_code}")
